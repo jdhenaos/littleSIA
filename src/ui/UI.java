36adfaf9f45ee1031359d7c0a3d0677d26a48f79
Rengifo00 <afrengifop@unal.edu.co>
2015-09-09 22:41:29 -0500

    Update UI.java

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,5 +1,33 @@
 package ui;
 
+import java.util.ArrayList;
+
+import data.Carrera;
+import data.Curso;
+import data.Departamento;
+import data.Estudiante;
+import data.Facultad;
+import data.Grupo;
+import data.Nota;
+import data.Profesor;
+import data.Sede;
+
 public class UI {
+	
+	private String Separador= "______________________________"; 
+	
+	public void imprimirSede(Sede sede){
+		
+		System.out.print(sede.toString());	
+	}
+	
+	public void Inicio(ArrayList<Sede> sedes){
+		System.out.println("Bienvenidos al miniSIA, escoga su sede:\n"+Separador+"\n");
+		
+		int index=0;
+		for(Sede s:sedes)
+			System.out.print("["+ ++index +"] "+s.getNombre()+"\n");
+	}
 
 }
+
